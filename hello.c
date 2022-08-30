30ba4ae1c63e11a11ff9574b6c0299d7af44f169
Bentley Fanslow <bfanslow@unomaha.edu>
2022-08-30 04:01:33 +0000

    changes made

--- hello.c
+++ hello.c
@@ -1,4 +1,21 @@
-5
-7
-645
-53
+/**
+ * Author: Jackson Goodman
+ *
+ * Date: 2022/08/29
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello I'm Jackson Goodman\n");
+  printf("My Major is Electrical Engineering\n");
+  printf("And I'm Bentley Fanslow\n");
+  printf("My Major is also Electrical Engineering\n");
+
+
+  return 0;
+}
